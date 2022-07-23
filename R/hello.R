ad01f402a2c2f785fe0fef393f31c771c18f2d6d
nutritionviz <99536034+nutritionviz@users.noreply.github.com>
2022-07-23 16:10:12 +0100

    algo

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,31 @@
+library(caret)
+library(combinat)
 
 labelremap <- function(cluster, truth) {
+  # input validation
+  if (length(cluster)!= length(truth)) {
+    stop(paste(
+        "Factors are not of equal length:",
+        length(cluster),
+        length(truth)
+    ))
+  }
 
-  print("Hello, world! Github")
+  # find best permutation
+  max_acc <- 0
+  best_labels <- c()
+  x <- levels(truth)
+  for (perm in permn(x)) {
+    cluster_temp <- cluster
+    levels(cluster_temp) <- perm
+    cluster_temp = as.numeric(levels(cluster_temp)[as.integer(cluster_temp)])
+    cluster_temp <- as.factor(cluster_temp)
+    cfm <- confusionMatrix(cluster_temp, truth)
+
+    if (cfm$overall[['Accuracy']] > max_acc) {
+      best_labels <- cluster_temp
+    }
+  }
+
+  return(best_labels)
 }
